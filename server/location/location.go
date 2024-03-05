4a0f419659c382a042dbd2f816131f186095c315
Yatharth Bhargava <yatharthbhargava@gmail.com>
2024-03-05 01:44:21 -0500

    finished location.go

--- server/location/location.go
+++ server/location/location.go
@@ -1,21 +1,91 @@
 package location
 
-type AreaOfInterest struct {
-	lowerLeftCoord Coord
-	upperRightCoord Coord
-	clients map[int]bool{}
-	clientlist []int 
-
-	Belongs(coord Coord) bool 
-	Clients() []int 
+type Coord struct {
+	Latitude  float64
+	Longitude float64
 }
 
+type AreaOfInterest struct {
+	lowerLeftCoord  Coord
+	upperRightCoord Coord
+	clients         map[int]bool
+	clientlist      []int
+}
+
+func (aoi *AreaOfInterest) Belongs(coord Coord) bool {
+	if coord.Latitude >= aoi.lowerLeftCoord.Latitude && coord.Latitude <= aoi.upperRightCoord.Latitude &&
+		coord.Longitude >= aoi.lowerLeftCoord.Longitude && coord.Longitude <= aoi.upperRightCoord.Longitude {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (aoi *AreaOfInterest) Clients(coord Coord) []int {
+	var activeClients []int
+	for _, clientID := range aoi.clientlist {
+		if isActive, exists := aoi.clients[clientID]; exists && isActive {
+			activeClients = append(activeClients, clientID)
+		}
+	}
+	return activeClients
+}
 
 type LocationTracker struct {
 	areaOfInterestList []AreaOfInterest
-	
+}
 
-	RegisterLocation (clientId int, coord Coord)
-	LookupAreaOfInterest (coord Coord) AreaOfInterst
-	RelatedVehicles (clientId int) 
+func (lt *LocationTracker) RegisterLocation(clientId int, coord Coord) *AreaOfInterest {
+	var clientAoI *AreaOfInterest
+
+	for i, aoi := range lt.areaOfInterestList {
+		clientInAoi, exists := aoi.clients[clientId]
+
+		if exists && clientInAoi {
+			lt.areaOfInterestList[i].clients[clientId] = false
+		}
+
+		if aoi.Belongs(coord) && clientAoI == nil {
+			if !exists {
+				lt.areaOfInterestList[i].clientlist = append(lt.areaOfInterestList[i].clientlist, clientId)
+			}
+			lt.areaOfInterestList[i].clients[clientId] = true
+			clientAoI = &lt.areaOfInterestList[i]
+		}
+
+	}
+
+	return clientAoI
+}
+
+func (lt *LocationTracker) LookupAreaOfInterest(coord Coord) AreaOfInterest {
+	var aoiFound AreaOfInterest
+
+	for _, aoi := range lt.areaOfInterestList {
+
+		if aoi.Belongs(coord) {
+			return aoi
+		}
+	}
+
+	return aoiFound
+}
+
+func (lt *LocationTracker) RelatedVehicles(clientId int) []int {
+	var res []int
+	for _, aoi := range lt.areaOfInterestList {
+		clientInAoi, exists := aoi.clients[clientId]
+
+		if exists && clientInAoi {
+			for _, client := range aoi.clientlist {
+				related, _ := aoi.clients[client]
+				if related {
+					res = append(res, client)
+				}
+			}
+
+			return res
+		}
+	}
+	return res
 }
